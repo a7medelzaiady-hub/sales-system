d54b1bd4edc39bdbbabab34eaf4602578ef3bdf9
a7medelzaiady-hub <a7medelzaiady@gmail.com>
2026-05-13 23:29:39 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,54 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker Installed');
+const CACHE_NAME = "elzaiady-v1";
+
+const urlsToCache = [
+  "/",
+  "/login.html",
+  "/index1.html",
+  "/sales-history.html"
+];
+
+// تثبيت الكاش
+self.addEventListener("install", (event) => {
+  console.log("Service Worker Installed");
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  // بيسمح للتطبيق يشتغل حتى لو النت ضعيف
+// تشغيل أوفلاين
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) return response;
+
+      return fetch(event.request)
+        .then((networkResponse) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+        })
+        .catch(() => {
+          return caches.match("/login.html");
+        });
+    })
+  );
+});
+
+// تحديث الكاش
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
 });
