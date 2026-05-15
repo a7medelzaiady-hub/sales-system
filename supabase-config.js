021edee69817af31f4677e5d367754e6784bddf6
a7medelzaiady-hub <a7medelzaiady@gmail.com>
2026-05-15 20:01:03 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,7 +1,14 @@
-const SUPABASE_URL = "ضع رابط مشروع Supabase";
-const SUPABASE_KEY = "ضع anon public key";
+// Supabase Configuration File
+// Replace values with your own from Supabase dashboard
 
+const SUPABASE_URL = "https://fqthxzaorgroaobvarov.supabase.co";
+const SUPABASE_KEY = "sb_publishable_eGRAc88JCypa0-jYFhL8BA_AwCs6gg0";
+
+// Create Supabase client
 const supabaseClient = supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
+
+// You can test connection (optional)
+console.log("Supabase connected:", SUPABASE_URL);
